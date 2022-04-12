be2a5af8294e103be2a88080ddb8a956e19c7b74
Kiril Komitski <k.komitski94@gmail.com>
2022-04-12 11:54:01 +0100

    complete setup

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,10 @@
-import Head from 'next/head';
 import React from 'react';
+import Meta from '../components/Meta';
 
 export default function about() {
 	return (
 		<div>
-			<Head>
-				<title>About</title>
-			</Head>
+			<Meta title='about' />
 			<h1>about</h1>
 		</div>
 	);
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { server } from '../../../config/index';
+
 const Article = ({ article }) => {
 	// const router = useRouter();
 	// const { id } = router.query;
@@ -18,8 +20,32 @@ const Article = ({ article }) => {
 	);
 };
 
+// export const getStaticProps = async (context) => {
+// 	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+// 	const article = await res.json();
+
+// 	return {
+// 		props: {
+// 			article,
+// 		},
+// 	};
+// };
+
+// export const getStaticPaths = async () => {
+// 	const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// 	const articles = await res.json();
+
+// 	const ids = articles.map((article) => article.id);
+// 	const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+// 	return {
+// 		paths,
+// 		fallback: false,
+// 	};
+// };
+
 export const getStaticProps = async (context) => {
-	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+	const res = await fetch(`${server}/api/articles/${context.params.id}`);
 	const article = await res.json();
 
 	return {
@@ -30,7 +56,7 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-	const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+	const res = await fetch(`${server}/api/articles`);
 	const articles = await res.json();
 
 	const ids = articles.map((article) => article.id);
